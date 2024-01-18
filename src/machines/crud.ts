b1bf0624ced89bc20e45fc62663f5b04c78de89f
laurakalbag <me@laurakalbag.com>
2024-01-18 12:48:17 +0000

    Update crud machine

--- src/machines/crud.ts
+++ src/machines/crud.ts
@@ -4,9 +4,9 @@ export const machine = createMachine(
   {
     /** @xstate-layout N4IgpgJg5mDOIC5QGMBOBXCA6AYgS1VgBcACAZSIEMiwBiAOzAA8iBtABgF1FQAHAe1h4iefvR4gmiAIwB2dlgBs7AJwAmFdICsAFmnSAzFpUAaEAE9Eag1i2yda6QA5ZWgL4ez9fhDgS0mBICQiJiElIIALQGilhq7A5qWoYGKroqTmaWUVpaWA52TtJJniAB2PiEpBTUYEGCwqLiSJIy0liyabL2TorKOlrsallWarb2ji7ubmblWGRgyGIQ5FQ09SFN4VYqsbLK9gbx9uz2IwhqY3YOzq4eHkA */
     id: "crud",
-    initial: "First State",
+    initial: "Here's a first state",
     states: {
-      "First State": {
+      "Here's a first state": {
         on: {
           next: {
             target: "Second State",
