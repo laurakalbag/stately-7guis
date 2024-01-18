9e5092c77f09df911a8a79a58e6efc70db7a276a
Laura Kalbag <me+github@laurakalbag.com>
2024-01-18 12:38:21 +0000

    Auto commit from Stately Studio

---
+++ src/machines/timer.ts
@@ -0,0 +1,26 @@
+import { createMachine } from "xstate";
+
+export const machine = createMachine(
+  {
+    /** @xstate-layout N4IgpgJg5mDOIC5QBcCWBbMAnAdAMVS1mQAIBlZAQ2TAGIA7MAD2QG0AGAXUVAAcB7WKjT96PEE0QBGAOzscANnYBOAEzKpAVgAsUqQGZNygDQgAnolX6cmmdtVSAHDM0Bfd6fr8IccWkxY4gJCImJIEogAtPoKOKrs9qqaBvrKOsqOphYIkZqaOPa2jlJJHiD+2PiExORUNEGCwqii4pIIejgyaTJ2jgpK2prsqlmWqjZ2Ds5urqYVuGRgAMaiELXUYA0hzWGgbeqxMkp2+vF27HajCKrjtvZOLu7uQA */
+    id: "timer",
+    initial: "First State",
+    states: {
+      "First State": {
+        on: {
+          next: {
+            target: "Second State",
+          },
+        },
+      },
+      "Second State": {},
+    },
+    types: { events: {} as { type: "next" } },
+  },
+  {
+    actions: {},
+    actors: {},
+    guards: {},
+    delays: {},
+  },
+);
