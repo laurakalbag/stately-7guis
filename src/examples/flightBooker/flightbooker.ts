952177c2474677be2f175cb6904b63e706715a62
Laura Kalbag <me+github@laurakalbag.com>
2024-01-18 12:14:12 +0000

    Auto commit from Stately Studio

--- src/examples/flightBooker/flightbooker.ts
+++ src/examples/flightBooker/flightbooker.ts
@@ -4,7 +4,9 @@ export const machine = createMachine(
   {
     /** @xstate-layout N4IgpgJg5mDOIC5QDMA2BLKALALgIQHsCBrMAJwDoAxdM2HAAgGUcBDHMAYgDswAPHAG0ADAF1EoAA4FY6HOgLcJIPogCMAdmEUAbMICcAJn1qArABY1agMyn9AGhABPRIesVTG84bUAODaYAvsGO3AQQcMpomLiEJOTK0rLyisqqCAC01joUhsLehqY21voW+r6OLpmmphTenr5qhSEg0dj4RKSUNHSMLOxgiTJyCkpIKupqFBqlGl6+OnrmpsKGla6GHl4+-kGBjm2xneQUTGAAxooQzGwcQ8mjaa76ORp6XtZ5XsJe6wiGm083j8AWCwSAA */
     id: "flightBooker",
+
     initial: "First State",
+
     states: {
       "First State": {
         on: {
@@ -15,7 +17,9 @@ export const machine = createMachine(
       },
       "Second State": {},
     },
+
     types: { events: {} as { type: "next" } },
+    description: `Machine Two`
   },
   {
     actions: {},
