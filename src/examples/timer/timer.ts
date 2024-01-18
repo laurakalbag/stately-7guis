3a345ba63586b0ba88a1c4bf3d221e22278144b8
laurakalbag <me@laurakalbag.com>
2024-01-18 12:16:38 +0000

    Add barebones timer machine

---
+++ src/examples/timer/timer.ts
@@ -0,0 +1,28 @@
+import { createMachine } from "xstate";
+
+export const timer = createMachine(
+  {
+    id: "flightBooker",
+
+    initial: "First State",
+
+    states: {
+      "First State": {
+        on: {
+          next: {
+            target: "Second State",
+          },
+        },
+      },
+      "Second State": {},
+    },
+
+    types: { events: {} as { type: "next" } },
+  },
+  {
+    actions: {},
+    actors: {},
+    guards: {},
+    delays: {},
+  },
+);
