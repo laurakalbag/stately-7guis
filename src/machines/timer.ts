e25f659f7f384e10c5e76cdb820b13540ce2e5f5
laurakalbag <me@laurakalbag.com>
2024-01-18 12:39:45 +0000

    Update timer machine const name

--- src/machines/timer.ts
+++ src/machines/timer.ts
@@ -1,6 +1,6 @@
 import { createMachine } from "xstate";
 
-export const machine = createMachine(
+export const timerMachine = createMachine(
   {
     /** @xstate-layout N4IgpgJg5mDOIC5QBcCWBbMAnAdAMVS1mQAIBlZAQ2TAGIA7MAD2QG0AGAXUVAAcB7WKjT96PEE0QBGAOzscANnYBOAEzKpAVgAsUqQGZNygDQgAnolX6cmmdtVSAHDM0Bfd6fr8IccWkxY4gJCImJIEogAtPoKOKrs9qqaBvrKOsqOphYIkZqaOPa2jlJJHiD+2PiExORUNEGCwqii4pIIejgyaTJ2jgpK2prsqlmWqjZ2Ds5urqYVuGRgAMaiELXUYA0hzWGgbeqxMkp2+vF27HajCKrjtvZOLu7uQA */
     id: "timer",
