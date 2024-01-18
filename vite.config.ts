7240279f90906444ae3b845232a0850cbd643c63
laurakalbag <me@laurakalbag.com>
2024-01-18 18:05:15 +0000

    Add Stately Sky

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // We need this here to ensure changes to the .env file are picked up while running in dev mode
+  optimizeDeps: { include: ['@statelyai/sky', '@statelyai/sky-react'] },
 })
