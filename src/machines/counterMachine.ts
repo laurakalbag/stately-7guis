1aecd2bf1e70c4cfa8bb3399f50829e709a149dc
laurakalbag <me@laurakalbag.com>
2024-01-22 16:30:29 +0000

    Use Sky with counterMachine

--- src/machines/counterMachine.ts
+++ src/machines/counterMachine.ts
@@ -1,7 +1,11 @@
-import { actorFromStately } from '@statelyai/sky';
+import { skyConfig } from "./counterMachine.sky";
+import { actorFromStately } from "@statelyai/sky";
 
-export const counterMachine = actorFromStately({
-  apiKey: 'sta_67b85e94-f83d-4b88-8ef6-2250a15afc09',
-  url: 'https://sky.stately.ai/NNoyE8',
-  sessionId: 'shared-session',
-});
+export const counterMachine = actorFromStately(
+  {
+    apiKey: "sta_67b85e94-f83d-4b88-8ef6-2250a15afc09",
+    url: "https://sky.stately.ai/NNoyE8",
+    sessionId: "shared-session",
+  },
+  skyConfig
+);
