d84905faecccc75795fe382d3b465537cd46790c
laurakalbag <me@laurakalbag.com>
2024-01-22 15:33:59 +0000

    Add Sky setup and start converting machine to Sky

--- src/machines/counterMachine.ts
+++ src/machines/counterMachine.ts
@@ -1,31 +1,7 @@
-import { createMachine, assign } from "xstate";
+import { actorFromStately } from '@statelyai/sky';
 
-export const counterMachine = createMachine(
-  {
-    /** @xstate-layout N4IgpgJg5mDOIC5QGMD2BXAdgFzAJwGIBJAOQGEAlAUQFkqSAVAbQAYBdRUAB1VgEtsfVJk4gAHogC0AJgBsAOmkBWAOwAOAIzKANCACeUrQGZ5Ro9JZGtSgL53dmVBDii0WXHlE9+g4aIkILLoGgXZ2QA */
-    context: {
-      count: 0,
-    },
-    id: "counter",
-    on: {
-      INCREMENT: {
-        target: "#counter",
-        actions: {
-          type: "increment",
-        },
-      },
-    },
-    types: {
-      events: {} as { type: "INCREMENT" },
-      context: {} as { count: number },
-    },
-  },
-  {
-    actions: {
-      increment: assign({ count: ({ context }) => context.count + 1 }),
-    },
-    actors: {},
-    guards: {},
-    delays: {},
-  },
-);
+export const counterMachine = actorFromStately({
+  apiKey: 'sta_67b85e94-f83d-4b88-8ef6-2250a15afc09',
+  url: 'https://sky.stately.ai/NNoyE8',
+  sessionId: 'shared-session',
+});
